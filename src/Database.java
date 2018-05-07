a3c8d56758cb9899c5c64f0804af95db0b02ceeb
David Lee <chazz681@gmail.com>
2018-05-06 20:08:55 -0400

    finished my delete

--- src/Database.java
+++ src/Database.java
@@ -42,6 +42,118 @@ class Database {
             System.out.println();//Move to the next line to print the next row.
         }
     }
+    private static void DeleteTuple(Connection conn) throws SQLException {
+    	String table;
+    	String user_input;
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Which Table?");
+    	table = scanner.nextLine();
+    	table = table.toUpperCase();
+    	if (table.equals("RESERVATION")) {
+    		System.out.println("What is the r_num?");
+    		user_input = scanner.nextLine();
+    		int r_num_int = Integer.parseInt(user_input);
+    		String hold = "DELETE FROM " +table+ " WHERE r_num = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, r_num_int);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("CUSTOMER")) {
+    		System.out.println("What is the CID?");
+    		user_input = scanner.nextLine();
+    		int cid_int = Integer.parseInt(user_input);
+    		String hold = "DELETE FROM " +table+ " WHERE cid = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, cid_int);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("HOTEL")) {
+    		System.out.println("What is the branch id?");
+    		user_input = scanner.nextLine();
+    		int id_int = Integer.parseInt(user_input);
+    		System.out.println("What is the hotel name?");
+    		user_input = scanner.nextLine();
+    		String hotel_name = user_input;
+    		String hold = "DELETE FROM " +table+ " WHERE id = ? and hotel_name = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, id_int);
+    		st.setString(2, hotel_name);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("ROOM")) {
+    		System.out.println("What is the room type?");
+    		user_input = scanner.nextLine();
+    		String hold = "DELETE FROM " +table+ " WHERE room_type = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setString(1, user_input);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("DATELIST")) {
+    		System.out.println("What is the date?");
+    		user_input = scanner.nextLine();
+    		String hold = "DELETE FROM " +table+ " WHERE my_date = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setString(1, user_input);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("RESERVATION_ON")) {
+    		System.out.println("What is the branch id?");
+    		user_input = scanner.nextLine();
+    		int id_int = Integer.parseInt(user_input);
+    		System.out.println("What is the hotel name?");
+    		user_input = scanner.nextLine();
+    		String hotel_name = user_input;
+    		String hold = "DELETE FROM " +table+ " WHERE id = ? and hotel_name = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, id_int);
+    		st.setString(2, hotel_name);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("RESERVATION_CHECK_IN")) {
+    		System.out.println("What is the r_num?");
+    		user_input = scanner.nextLine();
+    		int r_num_int = Integer.parseInt(user_input);
+    		String hold = "DELETE FROM " +table+ " WHERE r_num = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, r_num_int);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("RESERVATION_CHECK_OUT")) {
+    		System.out.println("What is the r_num?");
+    		user_input = scanner.nextLine();
+    		int r_num_int = Integer.parseInt(user_input);
+    		String hold = "DELETE FROM " +table+ " WHERE r_num = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, r_num_int);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("DATE_LIST_INFO")) {
+    		System.out.println("What is the branch id?");
+    		user_input = scanner.nextLine();
+    		int id_int = Integer.parseInt(user_input);
+    		System.out.println("What is the hotel name?");
+    		user_input = scanner.nextLine();
+    		String hotel_name = user_input;
+    		String hold = "DELETE FROM " +table+ " WHERE id = ? and hotel_name = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, id_int);
+    		st.setString(2, hotel_name);
+    		st.executeUpdate();
+    	}
+    	if (table.equals("HAS")) {
+    		System.out.println("What is the branch id?");
+    		user_input = scanner.nextLine();
+    		int id_int = Integer.parseInt(user_input);
+    		System.out.println("What is the hotel name?");
+    		user_input = scanner.nextLine();
+    		String hotel_name = user_input;
+    		String hold = "DELETE FROM " +table+ " WHERE id = ? and hotel_name = ?";
+    		PreparedStatement st = conn.prepareStatement(hold);
+    		st.setInt(1, id_int);
+    		st.setString(2, hotel_name);
+    		st.executeUpdate();
+    	}
+    }
 
     static void executeOption(String option, Connection conn, String userid) throws SQLException {
         switch(option){
@@ -56,6 +168,7 @@ class Database {
             case "ut":
                 break;
             case "dt":
+            	DeleteTuple(conn);
                 break;
             case "gr":
                 break;
